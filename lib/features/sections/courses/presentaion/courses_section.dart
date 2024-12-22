b6347813f3408d01621ea544dcd6938d82a1294c
Mohammed Al Zumair <mrz.m7md@gmail.com>
2024-12-22 15:20:22 +0300

    تصليح course section

--- lib/features/sections/courses/presentaion/courses_section.dart
+++ lib/features/sections/courses/presentaion/courses_section.dart
@@ -1,3 +1,5 @@
+import 'package:course_dashboard/core/values/images.dart';
+import 'package:course_dashboard/features/sections/courses/business/cubit_controller/courses_cubit.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -16,10 +18,12 @@ class CoursesSection extends StatefulWidget {
 
 class _CoursesSectionState extends State<CoursesSection> {
   late TextEditingController searchController;
-
+  late CoursesCubit coursesCubit;
+  
   @override
   void initState() {
     super.initState();
+    coursesCubit = CoursesCubit.get(context)..getCourses(keywordSearch: "") ;
     searchController = TextEditingController();
   }
 
@@ -59,74 +63,92 @@ class _CoursesSectionState extends State<CoursesSection> {
               ),
               Container(
                 alignment: AlignmentDirectional.topStart,
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Padding(
-                    padding: EdgeInsets.all(
-                        smallVerticalPadding(
-                            context: context)),
-                    child: DataTable(
-                      // headingRowColor: WidgetStatePropertyAll(Colors.red),
-                      dividerThickness: 0,
-                      columnSpacing: 100,
-                      dataRowColor: WidgetStateProperty.all(
-                          Colors.white),
-                      columns: <DataColumn>[
-                        appDataColumnWidget(context: context, title: "خيارات"),
-                        appDataColumnWidget(context: context, title: "الاسم"),
-                        appDataColumnWidget(context: context, title: "الصنف"),
-                        appDataColumnWidget(context: context, title: "تاريخ الإنشاء"),
-                      ],
-                      rows: List<DataRow>.generate(
-                        100,
-                            (index) => DataRow(
-                          color: WidgetStatePropertyAll(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Padding(
+                        padding: EdgeInsets.all(
+                            smallVerticalPadding(
+                                context: context)),
+                        child: DataTable(
+                          // headingRowColor: WidgetStatePropertyAll(Colors.red),
+                          dividerThickness: 0,
+                          // columnSpacing: 10,
+                          dataRowColor: WidgetStateProperty.all(
                               Colors.white),
-                          // color: WidgetStatePropertyAll(Colors.red),
-                          cells: <DataCell>[
-                            DataCell(
-                              Row(
-                                children: [
-                                  IconButton(onPressed: (){}, icon: Icon(Icons.edit),),
-                                  SizedBox(
-                                    width: 10,
-                                  ),
-                                  IconButton(onPressed: (){}, icon: Icon(CupertinoIcons.delete),),
-                                ],
-                              ),
-                            ),
-                            appDataCellWidget(context: context, title: "محمد أحمد سعيد"),
-                            appDataCellWidget(context: context, title: "الكهربائيات"),
-                            appDataCellWidget(context: context, title: "1 - 1 - 2024"),
+                          columns: <DataColumn>[
+                            appDataColumnWidget(context: context, title: "خيارات"),
+                            appDataColumnWidget(context: context, title: "        "),
+                            appDataColumnWidget(context: context, title: "المعرف"),
+                            appDataColumnWidget(context: context, title: "صورة العنوان"),
+                            appDataColumnWidget(context: context, title: "العنوان"),
+                            appDataColumnWidget(context: context, title: "الحالات"),
                           ],
+                          rows: List<DataRow>.generate(
+                            5,
+                                (index) => DataRow(
+                              color: WidgetStatePropertyAll(Colors.white),
+                              // color: WidgetStatePropertyAll(Colors.red),
+                              cells: <DataCell>[
+                                DataCell(
+                                  Row(
+                                    children: [
+                                      IconButton(onPressed: (){}, icon: Icon(Icons.edit),),
+                                      SizedBox(
+                                        width: 10,
+                                      ),
+                                      IconButton(onPressed: (){}, icon: Icon(CupertinoIcons.delete),),
+                                    ],
+                                  ),
+                                ),
+                                appDataCellWidget(context: context, title: ""),
+                                appDataCellWidget(context: context, title: "1"),
+                                appDataCellWidget(context: context, title: "1 - 1 - 2024"),
+                                appDataCellWidget(context: context, title: "1 - 1 - 2024"),
+                                DataCell(
+                                  Row(
+                                    children: [
+                                      Image.asset(lockImage, width: 30,),
+                                      const SizedBox(
+                                        width: 20,
+                                      ),
+                                      Image.asset(downloadImage, width: 30,),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
                         ),
                       ),
                     ),
-                  ),
+                    SizedBox(
+                      height: 200,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 16.0),
+                        child: Row(
+                          mainAxisAlignment:
+                          MainAxisAlignment.center,
+                          children: [
+                            IconButton(
+                              icon: const Icon(Icons.arrow_back),
+                              onPressed: () {},
+                            ),
+                            Text('Page 1 / 22'),
+                            IconButton(
+                              icon: Icon(Icons.arrow_forward),
+                              onPressed: () {},
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
                 ),
               ),
-              SizedBox(
-                height: 200,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 16.0),
-                  child: Row(
-                    mainAxisAlignment:
-                    MainAxisAlignment.center,
-                    children: [
-                      IconButton(
-                        icon: const Icon(Icons.arrow_back),
-                        onPressed: () {},
-                      ),
-                      Text('Page 1 / 22'),
-                      IconButton(
-                        icon: Icon(Icons.arrow_forward),
-                        onPressed: () {},
-                      ),
-                    ],
-                  ),
-                ),
-              )
             ],
           ),
         ),
